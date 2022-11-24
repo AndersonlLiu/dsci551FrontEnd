410486eb2577613b0d52caa4c0bacb814a7ec528
AndersonlLiu <kechengliu16@163.com>
2022-11-23 18:14:19 -0800

    change file component

--- dsciProject/src/app/files/files.component.ts
+++ dsciProject/src/app/files/files.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ThisReceiver } from '@angular/compiler';
 
 @Component({
   selector: 'app-files',
@@ -9,10 +10,9 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 })
 export class FilesComponent implements OnInit {
   returnData: any = "";
+  returnInfo: any = "";
   searchForm!: FormGroup;
   path = "";
-  request_body =
-    {};
   database = "";
 
   constructor(private formBuilder: FormBuilder, private http: HttpClient) { }
@@ -25,28 +25,48 @@ export class FilesComponent implements OnInit {
   onSubmit() {
     this.database = this.searchForm.value.database;
     this.http.get<any>('https://dsci551-367122.uw.r.appspot.com/api/v1/ls?directory_path=%2F&db='+ this.database).subscribe(data => {
-      this.path += "%2F";
       this.returnData = data.children;
-      console.log(typeof(this.returnData));
-      console.log(this.returnData);
     })
+  }
+  clear() {
+    this.path =this.path.substring(0, this.path.lastIndexOf('%2F'));
+    if (this.path === "") {
+      this.path = "%2F";
+    }
+    console.log(this.path);
+    this.http.get<any>('https://dsci551-367122.uw.r.appspot.com/api/v1/ls?directory_path=' + this.path + '&db='+ this.database).subscribe(data => {
+        this.returnData = data.children;
+        console.log(this.returnData);
+      })
+
   }
 
   itemclick(child: string) {
-    console.log("workks")
     if (child.includes(".")) {
+      this.path += "%2F";
       this.path += child;
-      this.http.put<any>('https://dsci551-367122.uw.r.appspot.com/api/v1/cat?file_path=' + this.path,this.request_body + '&db=' + this.database).subscribe(data => {
+      console.log(this.path);
+      this.http.get<any>('https://dsci551-367122.uw.r.appspot.com/api/v1/cat?file_path=' + this.path + '&db=' + this.database).subscribe(data => {
         console.log(data)
         console.log(this.path);
+        if (typeof(data) === 'object') {
+          this.returnInfo = JSON.stringify(data, null, 4);
+        } else {
+        this.returnInfo = data;
+        }
       })
     } else {
-      this.path += child;
-      this.path += "%2F"
+      if (this.path === "%2F") {
+        this.path += child;
+      } else {
+        this.path += "%2F";
+        this.path += child;
+      }
       this.http.get<any>('https://dsci551-367122.uw.r.appspot.com/api/v1/ls?directory_path=' + this.path + '&db='+ this.database).subscribe(data => {
         this.returnData = data.children;
         console.log(this.returnData);
         console.log(this.path);
+
       })
     }
   }
